384170af30f9c294955bf81bd2ea31500edff46f
Ricky Santos <34746@longroad.ac.uk>
2014-09-09 11:04:55 +0100

    Input and output program (name)

---
+++ Name.py
@@ -0,0 +1,8 @@
+#Ricky Dos Santos
+#9/9/2014
+#second program
+
+#this is a variable that is storing the value entered by the user
+first_name =input("please enter your name: ")
+#this out puts the name in the format "hi (Name)"
+print("hi {0} !".format(first_name))
