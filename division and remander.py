bb0bf75bc30bb835327e3b8186c54151e2058f0c
Ricky Santos <34746@longroad.ac.uk>
2014-09-12 16:06:02 +0100

    started exercise 1

---
+++ division and remander.py
@@ -0,0 +1,16 @@
+#Ricky Santos
+#development exercise 1
+#12/09/2014
+
+number1 = float(input("please enter your first number: "))
+number2 = float(input("please enter your second number: "))
+#float( and int( change the string into numbers
+
+division = number1/number2
+remainder = number1%number2
+#/ == devide
+#% == remainder
+
+print (division)
+print (remainder)
+
