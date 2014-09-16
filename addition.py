dcc6c79caf95a5d58c16b9958d1ae0a071862c6b
Ricky Santos <34746@longroad.ac.uk>
2014-09-16 11:13:39 +0100

    finished temp convesion made flow diagram for streach challenge addiotion file for hw

---
+++ addition.py
@@ -0,0 +1,10 @@
+#Ricky Santos
+#pool  volume
+#14/09/2014
+
+length = int(input("please enter your length number: "))
+width = int(input("please enter your width number: "))
+depth =int(input("please enter your depth number: "))
+
+total = length * width * depth
+print (total)
--- temperature conversion.py
+++ temperature conversion.py
@@ -5,16 +5,16 @@
 import math
 import time
 
-fahrenheit = float(input("please enter the temperature in degrees Fahrenheit: "))
+fahrenheit = int(input("please enter the temperature in degrees Fahrenheit: "))
 #float allows decimal numbers.
 
 centigrade = (fahrenheit - 32)*(5/9)
-centigrade = round(centigrade,1)
+centigrade = round(centigrade,0)
 #round(,2) rounds the number to two decimal places asmost temperatures come out
 #as recurring decimals.
 
-print ("calculating.")
-time.sleep(5)
-# Delay for 1 minute (5 seconds)
-       
+print ("calculating...")
+time.sleep(1)
+# Delay for 1 seconds
+
 print ("{1} degrees fahrenheit is {0} degrees celsius".format(centigrade,fahrenheit))
